f983ee170c610ca9452a48bc657790f8c46049d0
Armin <65064947+ArminPP@users.noreply.github.com>
2024-05-23 21:53:19 +0200

    changed baudrate to 115200 | update to 10 Hz

--- src/M5StickC_GPS.cpp
+++ src/M5StickC_GPS.cpp
@@ -1,3 +1,47 @@
+/*
+
+###########################
+# M5Stack Unit GPS AT6558 #
+###########################
+
+SEE: 3.2 NMEA Extension in AT3340.ProductManual.pdf
+'''''''''''''''''''''''''''''''''''''''''''''''''''
+
+The changes can be stored permanetly in the internal flash.
+
+$PCAS02,100*1E/r/n   			// 10 hz
+$PCAS02,1000*2E/r/n 			// 1 Hz
+$PCAS01,5*19/r/n   			    // Baud Rate 115200
+$PCAS11,3*1E/r/n    		    // Dynamic Model: Automotive
+
+$PCAS00*01/r/n					//save configuration	
+
+$PCAS03,1,1,1,1,1,1,0,0*02/r/n	// GGA,GGL,GSA,GSV,RMC,VTG=on,ZDA=off
+$PCAS03,0,0,0,0,1,0,0,0*03/r/n	// RMC=on ONLY! (no $GPTXT,01,01,01,ANTENNA OPEN*25 ANYMORE !!!!!!)
+
+
+
+#################
+# U-BLOX NEO M7 #
+#################
+
+This device has no internal flash storage, always change the parameters during setup!
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+const unsigned char ubxRate1Hz[] PROGMEM = { 0x06,0x08,0x06,0x00,0xE8,0x03,0x01,0x00,0x01,0x00 };
+const unsigned char ubxRate5Hz[] PROGMEM = { 0x06,0x08,0x06,0x00,200,0x00,0x01,0x00,0x01,0x00 };
+const unsigned char ubxRate10Hz[] PROGMEM ={ 0x06,0x08,0x06,0x00,100,0x00,0x01,0x00,0x01,0x00 };
+
+
+
+
+
+The $PMTK commands are for the MTK chip.
+All ublox NEO configuration commands start with $PUBX (NMEA text commands). 
+There are additional configuration options you can set with UBX binary commands.
+
+*/
+
 #include <Arduino.h>
 #include <M5StickC.h>
 #include <M5GFX.h>
@@ -15,15 +59,52 @@ const lgfx::GFXfont *unitFont  = &fonts::FreeSans9pt7b;
 const lgfx::GFXfont *dateFont  = &fonts::FreeSans12pt7b;
 const lgfx::GFXfont *timeFont  = &fonts::FreeSans12pt7b;
 
+byte ClearConfig[] = {0xB5, 0x62, 0x06, 0x09, 0x0D, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x01, 0x19, 0x98};
+byte GPGLLOff[]    = {0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x2B};
+byte GPGSVOff[]    = {0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x03, 0x39};
+byte GPVTGOff[]    = {0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x05, 0x47};
+byte GPGSAOff[]    = {0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x32};
+byte GPGGAOff[]    = {0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x24};
+byte GPRMCOff[]    = {0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x04, 0x40};
+byte Navrate10hz[] = {0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0x64, 0x00, 0x01, 0x00, 0x01, 0x00, 0x7A, 0x12};
+byte baud115200[]  = {0xb5, 0x62, 0x06, 0x00, 0x14, 0x00, 0x01, 0x00, 0x00, 0x00, 0xd0, 0x08, 0x00, 0x00, 0x00, 0xc2, 0x01,
+                      0x00, 0x07, 0x00, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0xc4, 0x96, 0xb5, 0x62, 0x06, 0x00, 0x01};
+
 // forward declarations
 void showGPSvalues();
+void sendPacket(byte *packet, byte len);
 
 void setup()
 {
     M5.begin();
 
     Serial.begin(115200);
+
+    delay(2000); // wait, if car is started ...
+
+    Serial.println("GNSS: starting with 9600 baud");
     GPS_SER.begin(9600, SERIAL_8N1, 32, 33);
+
+    sendPacket(baud115200, sizeof(baud115200));
+    delay(100);
+
+    Serial.println("GNSS: set to 115200 baud");
+    GPS_SER.updateBaudRate(115200);
+
+    Serial.println("GNSS: disable not needed messages");
+    delay(100);
+    sendPacket(GPGLLOff, sizeof(GPGLLOff));
+    delay(100);
+    sendPacket(GPGSVOff, sizeof(GPGSVOff));
+    delay(100);
+    sendPacket(GPVTGOff, sizeof(GPVTGOff));
+    delay(100);
+    sendPacket(GPGSAOff, sizeof(GPGSAOff));
+    delay(100);
+    sendPacket(GPGGAOff, sizeof(GPGGAOff));
+    delay(100);
+    sendPacket(Navrate10hz, sizeof(Navrate10hz));
+    delay(100);
 
     TFT.begin();
     TFT.setRotation(3);
@@ -43,7 +124,9 @@ void loop()
 
     while (GPS_SER.available() > 0)
     {
-        if (GPS.encode(GPS_SER.read()))
+        char c = GPS_SER.read();
+        Serial.write(c); // write raw data from GPS to console
+        if (GPS.encode(c))
         {
             showGPSvalues();
         }
@@ -51,9 +134,9 @@ void loop()
 
     if (millis() > 5000 && GPS.charsProcessed() < 10)
     {
-        Serial.println(F("No GPS detected: check wiring."));
-        while (true)
-            ;
+        Serial.println(F("------------------------> No GPS detected: check wiring."));
+        // while (true)
+        //     ;
     }
 
     if (M5.Axp.GetVBusVoltage() < 2.5) // USB power is plugged off
@@ -80,6 +163,19 @@ void loop()
 
         M5.Axp.PowerOff();
     }
+}
+
+void sendPacket(byte *packet, byte len)
+{
+    Serial.print("\r\n-----------> Packet sent to u-blox: ");
+    for (byte i = 0; i < len; i++)
+    {
+        GPS_SER.write(packet[i]);
+
+        Serial.print(packet[i], HEX);
+        Serial.print(F(" "));
+    }
+    Serial.println();
 }
 
 void showGPSvalues()
